dda073a6c70aa7a27a904b74f4663d8329507c5d
EStoilov <emkata_87@mail.bg>
2019-01-04 18:17:16 +0200

    add logick

---
+++ src/BuilderPattern.java
@@ -0,0 +1,8 @@
+public class BuilderPattern {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).
+                calories(100).sodium(35).carbohydrate(27).build();
+
+        System.out.println(cocaCola);
+    }
+}
